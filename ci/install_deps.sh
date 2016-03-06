a0e30036b4af57d06028640de6d15f86ed224770
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2016-03-06 17:29:13 -0500

    ci: Install cmocka for running unit tests

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -2,11 +2,16 @@
 # Install dependencies for Travis CI builds
 
 install_for_linux() {
+    # Since we're using the container-based infrastructure
+    # (sudo: false), we instead specify APT packages in .travis.yml,
+    # under addons.apt.packages
+    return
 }
 
 install_for_osx() {
     brew update
     brew install \
+        cmocka \
         glib
 }
 
